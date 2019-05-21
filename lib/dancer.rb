cadfd6f11cb7f3329806e4bc7b0c6a6fd3213a9c
Chet  Chopra <chopra.chet@gmail.com>
2019-05-21 18:16:01 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,7 +3,5 @@ require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 
 class Dancer
-  include Dance
-  extend FancyDance::ClassMethods
-  include FancyDance::InstanceMethods
+  extend MetaDancing
 end
